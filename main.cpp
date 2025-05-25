7d5ed02694150dcd7a5c4459e98573a728ae9691
jonathan hori <jonathanhori@gmail.com>
2025-05-24 22:36:00 -0400

    sdf

--- main.cpp
+++ main.cpp
@@ -14,20 +14,20 @@ struct CORS {
     struct context {};
 
     void before_handle(crow::request& req, crow::response& res, context&) {
-        res.add_header("Access-Control-Allow-Origin", "*");
+        /*res.add_header("Access-Control-Allow-Origin", "*");
         res.add_header("Access-Control-Allow-Headers", "Content-Type, Authorization");
         res.add_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.add_header("Access-Control-Allow-Credentials", "true");
         if (req.method == "OPTIONS"_method) {
             res.code = 204;
             res.end();
-        }
+        }*/
     }
     void after_handle(crow::request&, crow::response& res, context&) {
-        res.add_header("Access-Control-Allow-Origin", "*");
+        /*res.add_header("Access-Control-Allow-Origin", "*");
         res.add_header("Access-Control-Allow-Headers", "Content-Type, Authorization");
         res.add_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.add_header("Access-Control-Allow-Credentials", "true");
+        res.add_header("Access-Control-Allow-Credentials", "true");*/
     }
 };
 
